e8152d680483ed0b8222153d596ed7d91ff9ec80
Kenny Castaneda <kenny.castaneda@gmail.com>
2021-06-21 15:25:52 -0500

    more stripe changes

--- src/checkout/checkout.controller.js
+++ src/checkout/checkout.controller.js
@@ -4,15 +4,23 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 async function create(req, res) {
    const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
+      shipping_address_collection: {
+         allowed_countries: ["US", "CA"],
+      },
       line_items: [
          {
             price_data: {
                currency: "usd",
                product_data: {
-                  name: "hook razor",
+                  name: "Hook Razor",
                   images: ["https://www.hookrazor.com/images/hook-shop-4.jpg"],
                },
                unit_amount: 1500,
+            },
+            adjustable_quantity: {
+               enabled: true,
+               minimum: 1,
+               maximum: 10,
             },
             quantity: req.body.data,
          },
