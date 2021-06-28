eef63b726f9b151bccd33239e252dcd7a37b7d78
Kenny Castaneda <kenny.castaneda@gmail.com>
2021-06-28 10:52:07 -0500

    added white option

--- src/checkout/checkout.controller.js
+++ src/checkout/checkout.controller.js
@@ -1,30 +1,55 @@
 const stripe = require("stripe")(`${process.env.STRIPE_TEST}`);
+const PRICE = process.env.PRICE;
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function create(req, res) {
+   const { blackQuantity, whiteQuantity } = req.body.data;
+
+   let line_items = [];
+   if (blackQuantity > 0) {
+      line_items.push({
+         price_data: {
+            currency: "usd",
+            product_data: {
+               name: "Hook Razor Black",
+               images: ["https://www.hookrazor.com/images/hook-shop-3.jpg"],
+            },
+            unit_amount: PRICE,
+         },
+         adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+            maximum: 10,
+         },
+         quantity: blackQuantity,
+      });
+   }
+   if (whiteQuantity > 0) {
+      line_items.push({
+         price_data: {
+            currency: "usd",
+            product_data: {
+               name: "Hook Razor Black",
+               images: [
+                  "https://www.hookrazor.com/images/hook-shop-3-white.jpg",
+               ],
+            },
+            unit_amount: PRICE,
+         },
+         adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+            maximum: 10,
+         },
+         quantity: whiteQuantity,
+      });
+   }
    const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       shipping_address_collection: {
          allowed_countries: ["US", "CA"],
       },
-      line_items: [
-         {
-            price_data: {
-               currency: "usd",
-               product_data: {
-                  name: "Hook Razor",
-                  images: ["https://www.hookrazor.com/images/hook-shop-4.jpg"],
-               },
-               unit_amount: 1500,
-            },
-            adjustable_quantity: {
-               enabled: true,
-               minimum: 1,
-               maximum: 10,
-            },
-            quantity: req.body.data,
-         },
-      ],
+      line_items,
       mode: "payment",
       success_url: `${process.env.HOOK_DOMAIN}/success`,
       cancel_url: `${process.env.HOOK_DOMAIN}/bag`,
